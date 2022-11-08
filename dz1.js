4411573bae17072ae1d11d0ff9307619363305e2
Kirill Bereznikov <kirber228@gmail.com>
2022-11-08 14:39:26 +0700

    first

--- dz1.js
+++ dz1.js
@@ -1,7 +1,7 @@
 let a = [];
-let n = 100;
-let m = -5;
-let count = 100;
+let n = 2;
+let m = 5;
+let count = 50;
 
 for (let i = 0; i < count; i++ ) {
     a.push(Math.round(Math.random() * (m -n) + n));
